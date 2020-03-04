28062e174e05e7832be066df489e7bdeb1a2fab3
David <61717416+davidroth-br@users.noreply.github.com>
2020-03-04 18:37:12 -0500

    Added interest finder logic

--- script.py
+++ script.py
@@ -3,10 +3,6 @@ test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 attractions = [[] for i in destinations]
 
 def get_destination_index(destination):
-    #for i in range(len(destinations)):
-    #    if destinations[i] == destination:
-    #        destination_index = i
-    #return destination_index
     return destinations.index(destination)
 
 def get_traveler_location(traveler):
@@ -23,6 +19,17 @@ def add_attraction(destination, attraction):
     attractions_for_destination.append(attraction)
     return attractions_for_destination
 
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_the_city = attractions[destination_index]
+    attractions_with_interest = []
+    for possible_attraction in attractions_in_the_city:
+        attraction_tags = possible_attraction[1]
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+    return attractions_with_interest
+
 add_attraction("Los Angeles, USA", ["Venice Beach", ["beach"]])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
@@ -34,8 +41,6 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-print(attractions)
-#print(attractions)
-#test_destination_index = get_traveler_location(test_traveler)
-#print(test_destination_index)
-# print(get_destination_index("São Paulo, Brazil"))
+
+la_arts = find_attractions("Los Angeles, USA", ["art"])
+print(la_arts)
